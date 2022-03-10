5b076d0f5ba60304409dfa4cff4e31d1ec799481
Harish Kumar <Harish1.Kumar@radisys.com>
2022-03-10 16:51:27 +0530

    Config update

--- app/src/main/java/com/jio/testapplication/MainActivity.java
+++ app/src/main/java/com/jio/testapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.jio.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.jio.testlibrary.BluetoothDetails;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothDetails bluetoothDetails = new BluetoothDetails();
     }
 }

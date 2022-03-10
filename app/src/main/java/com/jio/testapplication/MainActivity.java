292900638ec72b368b2f35825b75aef55b4a0614
Harish Kumar <Harish1.Kumar@radisys.com>
2022-03-10 18:31:52 +0530

    build config changes

--- app/src/main/java/com/jio/testapplication/MainActivity.java
+++ app/src/main/java/com/jio/testapplication/MainActivity.java
@@ -3,14 +3,11 @@ package com.jio.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.jio.testlibrary.BluetoothDetails;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BluetoothDetails bluetoothDetails = new BluetoothDetails();
     }
 }
